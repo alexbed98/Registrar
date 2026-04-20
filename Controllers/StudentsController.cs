45cc317f9e042994326c5bfa601266d4beb42599
alexbed98 <alexbed98@gmail.com>
2026-04-20 13:56:11 -0400

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL;
+using EmailHandling;
+using Models;
+using System.Web.Mvc;
+using Registrar;
+using static Controllers.AccessControl;
+
+namespace Controllers
+{
+    public class StudentsController: Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
